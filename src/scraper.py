e3aee2545333b44ae25964f2b1bf08616c8821f3
Jason Walker <j26walker@gmail.com>
2019-03-09 18:44:28 -0500

    start scraping

---
+++ src/scraper.py
@@ -0,0 +1,37 @@
+import course
+from pprint import pprint
+import json
+
+class Course:
+    def __init__(self, id, className, major, classNumber, prereq, recitation, credit, description, coreq):
+        self.id = id
+        self.className = className
+        self.major = major
+        self.classNumber = classNumber
+        self.prereq = prereq
+        self.recitation = recitation
+        self.credits = credit
+        self.description = description
+        self.coreq = coreq
+
+
+# courses_file = open("classes.txt", "w")
+
+class_dict = course.get_term_courses(term="2201", subject="CS")
+cs_course = class_dict['1501']
+cs_section_list = cs_course.sections
+sect = cs_section_list[0].to_dict()
+for key in sect.items():
+    print(key)
+# pprint(cs_section_list)
+# print(type(sect._extra))
+
+# for sub in course.SUBJECTS:
+#     print(sub)
+#     class_dict = course.get_term_courses(term="2201", subject="CS")
+#     courses_file.write("------------------\n")
+#     courses_file.write(sub)
+
+#     for key, value in class_dict._courses.items():
+#         courses_file.write(key + "-" + value.title+"\n")
+#     courses_file.write("------------------\n")
