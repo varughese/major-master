f764eadde882aa849c2b414050ac895e462640aa
Jason Walker <j26walker@gmail.com>
2019-04-03 19:33:52 -0400

    add parse course descriptions

--- scraper/parser.py
+++ scraper/parser.py
@@ -6,14 +6,31 @@ cred = credentials.Certificate("secret_key.json")
 firebase_admin = firebase_admin.initialize_app(cred, {'databaseURL': 'https://major-master-20a62.firebaseio.com/'})
 # default_app = firebase_admin.initialize_app(cred)
 
-with open('data.json') as json_file:  
-    data = json.load(json_file)
-    for subject in data:
-        index = 0
-        for course in data[subject]:
-            ref = db.reference("course_list/" + subject + "/"+ str(index))
-            temp = {"name": course["className"],"id": course["id"]}
-            ref.set(temp)
-            index+=1
+def parseCourseList():
+    with open('data.json') as json_file:  
+        data = json.load(json_file)
+        for subject in data:
+            index = 0
+            for course in data[subject]:
+                ref = db.reference("course_list/" + subject + "/"+ str(index))
+                temp = {"name": course["className"],"id": course["id"]}
+                ref.set(temp)
+                index+=1
 
+def parseCourseDescriptions():
+    with open('data.json') as json_file:  
+        data = json.load(json_file)
+        for subject in data:
+            for course in data[subject]:
+                ref = db.reference("course_descriptions/" + course["id"])
+                temp = {
+                    "credits": course["credits"],
+                    "department": course["major"],
+                    "description": course["description"],
+                    "id": course["id"],
+                    "prereq": course["prereq"],
+                    "title": course["className"]
+                }
+                ref.set(temp)
 
+parseCourseDescriptions()
