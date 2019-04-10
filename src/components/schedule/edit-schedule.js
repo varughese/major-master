41f471ca399dfd1a1bfcab949f741cd2a3c3a790
Mathew Varughese <mat.varughese3@gmail.com>
2019-04-10 16:00:48 -0400

    stop redirect that was added

--- src/components/schedule/edit-schedule.js
+++ src/components/schedule/edit-schedule.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react';
 import WorkArea from './work-area';
-import * as ROUTES from '../../constants/routes';
-import firebase from "firebase/app";
 
 
 class EditSchedule extends Component {
-	
-	checkSignedIn = () => {
-		var user = firebase.auth().currentUser;
-		if(user == null) {
-			window.location.assign(ROUTES.SIGN_IN);
-		}
-	}
 	render() {
-		this.checkSignedIn();
 		return (
 			<WorkArea />
 		);
