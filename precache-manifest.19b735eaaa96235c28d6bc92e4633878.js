5934e2596f1a29b7a36cd39c4e6b69a5b2d178f4
Mathew Varughese <mat.varughese3@gmail.com>
2019-04-14 22:04:40 -0400

    Updates

---
+++ precache-manifest.19b735eaaa96235c28d6bc92e4633878.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "192305d0ea073cb2f2a8",
+    "url": "/major-master/static/js/runtime~main.bd31b7bd.js"
+  },
+  {
+    "revision": "63238e3c08024633e408",
+    "url": "/major-master/static/js/main.0d5470cd.chunk.js"
+  },
+  {
+    "revision": "cf2246fd2610ffe31237",
+    "url": "/major-master/static/js/2.90660f6e.chunk.js"
+  },
+  {
+    "revision": "63238e3c08024633e408",
+    "url": "/major-master/static/css/main.03f46888.chunk.css"
+  },
+  {
+    "revision": "9bf827f70cdf96a6d1c4736a989f1c46",
+    "url": "/major-master/index.html"
+  }
+];
